de7cb2cf8ac4809d5785aede76f6ccd98921e2ea
zsigab <zsigabeni@gmail.com>
2026-03-05 07:59:58 +0100

    Cleanup - keep single Dockerfile

--- docker/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# MicroProfile Config reads env vars automatically (SONARR_API_KEY -> sonarr.api-key).
-# This script is a simple passthrough with optional -Xmx tuning.
-exec java \
-  -Xmx100m \
-  -Dquarkus.http.enabled=false \
-  -jar /app/quarkus-run.jar
